681410c1794ab63f184d595e795ac4ec82d36e6e
Yasin Dalkılıç <y.dalkilic@cekino.com>
2020-07-29 21:18:11 +0300

    Updates

---
+++ precache-manifest.a24a6c401d571672900cd338b98c77a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1d3cf72f95e2a6f853e5bac229e57c3",
+    "url": "/react-chart-race/index.html"
+  },
+  {
+    "revision": "454f8b1a26c2c7490296",
+    "url": "/react-chart-race/static/css/main.0fdfc4c3.chunk.css"
+  },
+  {
+    "revision": "094dc2bdccf2deeb7427",
+    "url": "/react-chart-race/static/js/2.e2f82d03.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-chart-race/static/js/2.e2f82d03.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "454f8b1a26c2c7490296",
+    "url": "/react-chart-race/static/js/main.32c2a640.chunk.js"
+  },
+  {
+    "revision": "261ebf16c0766a827a26",
+    "url": "/react-chart-race/static/js/runtime-main.d852fcea.js"
+  }
+]);
